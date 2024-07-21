ae6f030974c0fa1c2a039413895317e739e95b3a
Queen Nashami <wanzanashami@gmail.com>
2024-07-21 04:51:55 +0300

    Lab Complete

--- lib/employee.py
+++ lib/employee.py
@@ -1,4 +1,5 @@
 # lib/employee.py
+
 from __init__ import CURSOR, CONN
 from department import Department
 
@@ -187,4 +188,10 @@ class Employee:
 
     def reviews(self):
         """Return list of reviews associated with current employee"""
-        pass
+        from review import Review  # avoid circular import issue
+        sql = """
+            SELECT * FROM reviews
+            WHERE employee_id = ?
+        """
+        rows = CURSOR.execute(sql, (self.id,)).fetchall()
+        return [Review.instance_from_db(row) for row in rows]
